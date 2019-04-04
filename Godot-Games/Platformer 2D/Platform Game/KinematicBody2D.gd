6147251b48e58f9bd14dbbfdf2aa526be70d9dff
Jeffwonderpouch <tmull_23@hotmail.com>
2019-04-03 19:49:19 -0600

    added double jump
    also added a picture of the game

--- Godot-Games/Platformer 2D/Platform Game/KinematicBody2D.gd
+++ Godot-Games/Platformer 2D/Platform Game/KinematicBody2D.gd
@@ -6,26 +6,27 @@ var motion = Vector2()
 var speed = 190
 
 # Jumping Variables
-var jumps = 2
+var jumps = 1 # Because of how is_on_floor method works, jumps = jumps + 1 (this is a double jump)
 var gravity = 9
-var jump_force = -390
+var jump_force = -350
 
 func _physics_process(delta):
 	motion.y += gravity;
 	if Input.is_action_pressed("ui_right"):
 		motion.x = speed
-		
 	elif Input.is_action_pressed("ui_left"):
 		motion.x = -speed
-		
 	else: 
 		motion.x = 0
-		
-	if jumps >= 1:
-		if Input.is_action_just_pressed("ui_up"):
-			motion.y = jump_force
-			jumps = jumps - 1
+	if Input.is_action_just_pressed("ui_up"):
+		if jumps >= 1:
+			if is_on_floor():
+				motion.y = jump_force
+				jumps = jumps - 1
+			else:
+				motion.y = jump_force
 		else:
-			motion.y += gravity
-
+			motion.y += gravity		
+	if is_on_floor():
+		jumps = 1
 	motion  = move_and_slide(motion, UP)
